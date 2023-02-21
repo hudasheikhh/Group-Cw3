30816353b7271bd7a5ca72aa0a8e30185696c8ac
H2203 <HS1145@live.mdx.ac.uk>
2023-02-21 16:14:56 +0400

    Added product.js file

---
+++ product.js
@@ -0,0 +1,23 @@
+// Defining 10 products as json objects 
+// Products is the json array object
+let products = [{
+        id: 1001,
+        subject: "English",
+        location: "Safa",
+        description: "blah blah blah",
+        price: 1000,
+        spaces: 5,
+        img: "images/English.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1002,
+        subject: "Math",
+        location: "Jumeirah",
+        price: 500,
+        spaces: 10,
+        img: "images/Math.jpg",
+        dSpace: 5,
+    }
+]
