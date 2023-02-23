2b878012c91c37a7ec258e5bdabf15d2ff20399e
H2203 <HS1145@live.mdx.ac.uk>
2023-02-23 13:35:01 +0400

    Added service-worker file and Update index.html

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+var cacheName = "lessons-v1";
+var cacheFiles = [
+    "index.html",
+    "Style.css",
+    "product.js",
+    "lessonstore.webmanifest",
+    "images",
+];
+
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log("[Service Worker] Caching all the files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// cache the third-party files
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(r) {
+            // Download the file if it is not in the cache,
+            return (
+                r ||
+                fetch(e.request).then(function(response) {
+                    // add the new file to cache
+                    return caches.open(cacheName).then(function(cache) {
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                })
+            );
+        })
+    );
+});
