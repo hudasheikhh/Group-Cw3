4bead34b40c963b0a80727519479f4896fe2c68e
H2203 <HS1145@live.mdx.ac.uk>
2023-02-23 13:41:07 +0400

    Updated product.js

--- product.js
+++ product.js
@@ -4,7 +4,6 @@ let products = [{
         id: 1001,
         subject: "English",
         location: "Safa",
-        description: "blah blah blah",
         price: 1000,
         spaces: 5,
         img: "images/English.jpg",
@@ -19,5 +18,85 @@ let products = [{
         spaces: 10,
         img: "images/Math.jpg",
         dSpace: 5,
-    }
+    },
+    {
+        id: 1003,
+        subject: "Social Studies",
+        location: "Rashidiya",
+        price: 500,
+        spaces: 5,
+        img: "images/Social Studies.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1004,
+        subject: "Computer Science",
+        location: "Karama",
+        price: 500,
+        spaces: 5,
+        img: "images/CS.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1005,
+        subject: "Science",
+        location: "JBR",
+        price: 2000,
+        spaces: 5,
+        img: "images/Science.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1006,
+        subject: "Psychology",
+        location: "Safa",
+        price: 500,
+        spaces: 5,
+        img: "images/Psych.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1007,
+        subject: "Business Studies",
+        location: "Jumeirah",
+        price: 500,
+        spaces: 5,
+        img: "images/Business.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1008,
+        subject: "Music",
+        location: "Rashidiya",
+        price: 1000,
+        spaces: 5,
+        img: "images/Music.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1009,
+        subject: "Moral Education",
+        location: "Karama",
+        price: 500,
+        spaces: 5,
+        img: "images/Moral.jpg",
+        dSpace: 5,
+
+    },
+    {
+        id: 1010,
+        subject: "Steam",
+        location: "JBR",
+        price: 5000,
+        spaces: 5,
+        img: "images/Steam.jpg",
+        dSpace: 5,
+
+    },
 ]
